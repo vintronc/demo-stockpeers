a16dd19bd4182de6b8c97b2a6faf406dd04bcadd
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-08-28 16:16:43 -0300

    Make the app more dashboard-like

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import altair as alt
 
 st.set_page_config(
     page_title="Stock peer group analysis",
-    page_icon=":chart_with_upwards_trend:"
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",
 )
 
 """
@@ -13,6 +14,8 @@ st.set_page_config(
 
 Easily compare stocks against others in their peer group.
 """
+
+""  # Add some space.
 
 STOCKS = [
     "AAPL",
@@ -128,19 +131,26 @@ if "tickers_input" not in st.session_state:
 # Callback to update query param when input changes
 def update_query_param():
     if st.session_state.tickers_input:
-        st.query_params["stocks"] = stocks_to_str(
-            st.session_state.tickers_input)
+        st.query_params["stocks"] = stocks_to_str(st.session_state.tickers_input)
     else:
         st.query_params.pop("stocks", None)
 
 
-# Input for stock tickers
-tickers = st.multiselect(
-    "Stock tickers",
-    options=sorted(set(STOCKS) | set(st.session_state.tickers_input)),
-    default=st.session_state.tickers_input,
-    accept_new_options=True,
-)
+cols = st.columns([1, 2])
+controls = cols[0].container(border=True, height="stretch")
+
+with controls:
+    """
+    #### Stocks to compare
+    """
+
+    # Input for stock tickers
+    tickers = st.multiselect(
+        "Stock tickers",
+        options=sorted(set(STOCKS) | set(st.session_state.tickers_input)),
+        default=st.session_state.tickers_input,
+        accept_new_options=True,
+    )
 
 tickers = [t.upper() for t in tickers]
 
@@ -156,20 +166,20 @@ if not tickers:
 
 # Time horizon selector
 horizon_map = {
-    "1 Week": "1wk",
     "1 Month": "1mo",
-    "3 Months": "3mo",
     "6 Months": "6mo",
     "1 Year": "1y",
     "5 Years": "5y",
     "10 Years": "10y",
-    "20 Years": "20y",
 }
-horizon = st.segmented_control(
-    "Time horizon",
-    options=list(horizon_map.keys()),
-    default="3 Months",
-)
+
+with controls:
+    horizon = st.segmented_control(
+        "Time horizon",
+        options=list(horizon_map.keys()),
+        default="6 Months",
+        width="stretch",
+    )
 
 
 @st.cache_resource(show_spinner=False)
@@ -191,40 +201,43 @@ if not len(data):
 # Normalize prices (start at 1)
 normalized = data.div(data.iloc[0])
 
-""
-""
 
 # Plot 1: Normalized prices
-"""
-### Normalized stock prices
-"""
+with cols[1].container(border=True):
+    """
+    #### Normalized price
+    """
 
-chart1 = (
-    alt.Chart(
-        normalized.reset_index().melt(
-            id_vars=["Date"], var_name="Stock", value_name="Normalized price"
+    chart1 = (
+        alt.Chart(
+            normalized.reset_index().melt(
+                id_vars=["Date"], var_name="Stock", value_name="Normalized price"
+            )
+        )
+        .mark_line()
+        .encode(
+            x="Date:T",
+            y="Normalized price:Q",
+            color=alt.Color("Stock:N", legend=alt.Legend(orient="bottom")),
         )
     )
-    .mark_line()
-    .encode(
-        x="Date:T",
-        y="Normalized price:Q",
-        color=alt.Color("Stock:N", legend=alt.Legend(orient="bottom")),
-    )
-    .properties(height=400)
-)
-st.altair_chart(chart1, use_container_width=True)
+
+    st.altair_chart(chart1, use_container_width=True)
 
 ""
 ""
 
 # Plot individual stock vs peer average
 """
-### Individual stocks vs peer average
-
-For the analysis below, the "peer average" when analyzing stock X always
-excludes X itself.
+#### Individual stocks vs peer average
 """
+
+st.caption(
+    """
+    For the analysis below, the "peer average" when analyzing stock X always
+    excludes X itself.
+    """
+)
 
 if len(tickers) <= 1:
     st.warning("Pick 2 or more tickers to compare them")
@@ -232,7 +245,10 @@ if len(tickers) <= 1:
 
 tabs = st.tabs(["Delta", "Price"])
 
-for ticker in tickers:
+NUM_COLS = 3
+tab_cols = [tab.columns(NUM_COLS) for tab in tabs]
+
+for i, ticker in enumerate(tickers):
     # Calculate peer average (excluding current stock)
     peers = normalized.drop(columns=[ticker])
     peer_avg = peers.mean(axis=1)
@@ -255,8 +271,10 @@ for ticker in tickers:
         .properties(title=f"{ticker} minus peer average", height=300)
     )
 
-    tabs[0].write("")
-    tabs[0].altair_chart(chart, use_container_width=True)
+    cell_num = i % NUM_COLS
+    cell = tab_cols[0][cell_num].container(border=True)
+    cell.write("")
+    cell.altair_chart(chart, use_container_width=True)
 
     # Create DataFrame with peer average.
     plot_data = pd.DataFrame(
@@ -285,5 +303,15 @@ for ticker in tickers:
         .properties(title=f"{ticker} vs Peer average", height=300)
     )
 
-    tabs[1].write("")
-    tabs[1].altair_chart(chart, use_container_width=True)
+    cell = tab_cols[1][cell_num].container(border=True)
+    cell.write("")
+    cell.altair_chart(chart, use_container_width=True)
+
+""
+""
+
+"""
+#### Raw data
+"""
+
+data
